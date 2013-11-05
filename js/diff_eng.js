b9132e605dd8c4abd095f6e5ff66e84c0fdd93b5
Nate Morse <morse.nate@gmail.com>
2013-11-05 17:50:00 -0500

    Update diff_eng.js
    use .content()

--- js/diff_eng.js
+++ js/diff_eng.js
@@ -1,27 +1,27 @@
 jQuery(function($) {
-var diff = function(obj) {
+  var diff = function(obj) {
     var attrs, index, attr;
     var elm = obj[0];
-    
+    if (elm.nodeType === 3) { // this is a text node
+      display("plaintext "+obj.text() + "<hr/>");
+      return;
+    }
     display(elm.nodeName + " :");
     attrs = elm.attributes;
     for (index = 0; index < attrs.length; ++index) {
       attr = attrs[index];
       display(attr.nodeName + " = " + attr.nodeValue);
     }
-    if (obj.children().length > 0) {
-      obj.children().each(function() {
-        diff($(this));
-      });
-    }
-    else {
-      display("text : "+obj.text() + "<hr/>");
-    }
+    
+    obj.contents().each(function() {
+      diff($(this));
+    });    
   };
-      $("div").each(function() {
-        diff($(this));
-      });
   function display(msg) {
     $("<p>").html(msg).appendTo(document.body);
   }
+  
+  $("div").each(function() {
+    diff($(this));
+  });
 });
