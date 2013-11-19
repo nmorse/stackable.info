9294d0a9aaf5c8c6a13b84e468d79b48c69f9ae6
nmorse <morse.nate@gmail.com>
2013-11-19 12:54:45 -0500

    compare refactor and some tests

--- js/diff_eng.js
+++ js/diff_eng.js
@@ -2,6 +2,7 @@ var comparable_diff = null;
 jQuery(function($) {
    comparable_diff = function(obj1, obj2, diff) {
     var parsed_attr_delta_lists = {};
+    var parsed_attr_delta_lists2 = {};
     var attrs1, attrs2, text1, text2;
     var elm1 = obj1[0];
     var elm2 = obj2[0];
@@ -32,7 +33,11 @@ jQuery(function($) {
         attrs2[index] = elm2.attributes[index];
     }
     
+    // use a symetry of attr_diff(a, b) to calculate attr.insertion as attr_diff(b, a).removal.
     parsed_attr_delta_lists = attr_diff(attrs1, attrs2);
+    parsed_attr_delta_lists2 = attr_diff(attrs2, attrs1);
+    parsed_attr_delta_lists.insertion = parsed_attr_delta_lists2.removal;
+    display_message("parsed_attr_delta_lists " + JSON.stringify(parsed_attr_delta_lists, null, " "));
     // do "value comparison" on matched attribute keys
     
     obj1.contents().each(function(i, o) {
@@ -59,75 +64,44 @@ jQuery(function($) {
       return -1;
   }
   function attr_diff(attrs1, attrs2) {
-    var attr1, attr2, text1, text2, index, i2 = 0, index2 = 0, pos;
-    var parsed_attr_delta_lists = {"removal":[], "key_match":[], "insertion":[]}, 
+    var attr1, attr2, text1, text2, index, found = 0, index2 = 0, pos;
+    var parsed_attr_delta_lists = {"removal":{}, "key_match":{}}, 
         comp;
     attrs1.sort(compare_nodeNames);
     attrs2.sort(compare_nodeNames);
-    display_message(" attr lengths "+ attrs1.length + " and " + attrs2.length);
+    //display_message(" attr lengths "+ attrs1.length + " and " + attrs2.length);
     for (index = 0; index < attrs1.length; index+=1) {
-        index2 = i2;
+        index2 = 0;
         attr1 = attrs1[index];
-        attr2 = attrs2[index2];
-        if (!attr2) { break; }
-        display_message(" attr diff for "+ attr1.nodeName + " and " + attr2.nodeName);
-        // make the attributes insertion and removal lists.
-        if (attr1.nodeName !== attr2.nodeName) {
-            // look ahead in the second list until a greater or matching element is found
-            //index2 += 1;
-            while (index2 < attrs2.length) {
-                // if (2) is greater (1) then add this attribute (1) to the removal list.
-                // or if a match, mark it to go through "value comparison"
-                // else if (2) is less than (1), add this attribute (1) to the insertion list.
-                attr2 = attrs2[index2];
-                comp = compare_nodeNames(attr2, attr1);
-                if (comp > 0) {
-                    parsed_attr_delta_lists.removal.push({"key":attr1.nodeName, "value":attr1.nodeValue});
-                    parsed_attr_delta_lists.insertion.push({"key":attr2.nodeName, "value":attr2.nodeValue});
-                    //break;
-                }
-                else if (comp === 0) {
-                    // nodeNames (aka attribute keys) are the same...
-                    // only add to delta lsit if the values are different. 
-                    if (attr1.nodeValue !== attr2.nodeValue) {
-                        parsed_attr_delta_lists.key_match.push({"key":attr1.nodeName, "values":[attr1.nodeValue, attr2.nodeValue]});
-                    }
-                    // remove from the removal or insertion lists if it was added
-                    pos = key_position(parsed_attr_delta_lists.removal, attr1);
-                    if (pos >= 0) {
-                        parsed_attr_delta_lists.removal.splice(pos, 1);
-                    }
-                    pos = key_position(parsed_attr_delta_lists.insertion, attr1);
-                    if (pos >= 0) {
-                        parsed_attr_delta_lists.insertion.splice(pos, 1);
-                    }
-                    i2 += 1;
-                    break;
-                }
-                else {
-                    parsed_attr_delta_lists.insertion.push({"key":attr2.nodeName, "value":attr2.nodeValue});
-                }
-                index2 += 1;
+        //display_message(" check all against attr1 "+ attr1.nodeName);
+        // look through the second list until a greater or matching element is found
+        found = 0;
+        while (index2 < attrs2.length) {
+            // if (2) is greater (1) then add this attribute (1) to the removal hash.
+            // or if a match, mark it to go through "value comparison"
+            // else if (2) is less than (1), add this attribute (1) to the insertion list.
+            attr2 = attrs2[index2];
+            comp = compare_nodeNames(attr2, attr1);
+            if (comp > 0) {
+                parsed_attr_delta_lists.removal[attr1.nodeName] = attr1.nodeValue;
+                break;
             }
+            else if (comp === 0) {
+                // nodeNames (aka attribute keys) are the same...
+                // only add to delta lsit if the values are different. 
+                if (attr1.nodeValue !== attr2.nodeValue) {
+                    parsed_attr_delta_lists.key_match[attr1.nodeName] = [attr1.nodeValue, attr2.nodeValue];
+                }
+                found = 1;
+                break;
+            }
+            index2 += 1;
         }
-        else {
-            if (attr1.nodeValue !== attr2.nodeValue) {
-                parsed_attr_delta_lists.key_match.push({"key":attr1.nodeName, "values":[attr1.nodeValue, attr2.nodeValue]});
-            }
-            // remove from the removal or insertion lists if it was added
-            // remove from the removal or insertion lists if it was added
-            pos = key_position(parsed_attr_delta_lists.removal, attr1);
-            if (pos >= 0) {
-                parsed_attr_delta_lists.removal.splice(pos, 1);
-            }
-            pos = key_position(parsed_attr_delta_lists.insertion, attr1);
-            if (pos >= 0) {
-                parsed_attr_delta_lists.insertion.splice(pos, 1);
-            }
-            i2 += 1;
+        if (!found) {
+            parsed_attr_delta_lists.removal[attr1.nodeName] = attr1.nodeValue;
         }
     }
-    display_message("parsed_attr_delta_lists " + JSON.stringify(parsed_attr_delta_lists, null, " "));
+    
     return parsed_attr_delta_lists;
   }
 });
