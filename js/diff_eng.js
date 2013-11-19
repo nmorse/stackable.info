19d35887d0a0b4312622375d113931e28dfeeaf9
nmorse <morse.nate@gmail.com>
2013-11-19 10:57:29 -0500

    compare (diff of) attributes

--- js/diff_eng.js
+++ js/diff_eng.js
@@ -1,7 +1,7 @@
 var comparable_diff = null;
 jQuery(function($) {
    comparable_diff = function(obj1, obj2, diff) {
-    var parsed_attr_lists = {};
+    var parsed_attr_delta_lists = {};
     var attrs1, attrs2, text1, text2;
     var elm1 = obj1[0];
     var elm2 = obj2[0];
@@ -32,7 +32,7 @@ jQuery(function($) {
         attrs2[index] = elm2.attributes[index];
     }
     
-    parsed_attr_lists = attr_diff(attrs1, attrs2);
+    parsed_attr_delta_lists = attr_diff(attrs1, attrs2);
     // do "value comparison" on matched attribute keys
     
     obj1.contents().each(function(i, o) {
@@ -48,21 +48,33 @@ jQuery(function($) {
   function compare_nodeNames(a,b) {
       return a.nodeName.localeCompare(b.nodeName);
   }
+  function key_position( arr, key) {
+      var i = 0;
+      while( i < arr.length) {
+          if (arr[i].key === key.nodeName) {
+              return i;
+          }
+          i += 1;
+      }
+      return -1;
+  }
   function attr_diff(attrs1, attrs2) {
-    var attr1, attr2, text1, text2, index, index2 = 0;
-    var parsed_attr_lists = {"removal":[], "key_match":[], "insertion":[]}, 
+    var attr1, attr2, text1, text2, index, i2 = 0, index2 = 0, pos;
+    var parsed_attr_delta_lists = {"removal":[], "key_match":[], "insertion":[]}, 
         comp;
     attrs1.sort(compare_nodeNames);
     attrs2.sort(compare_nodeNames);
-    
+    display_message(" attr lengths "+ attrs1.length + " and " + attrs2.length);
     for (index = 0; index < attrs1.length; index+=1) {
-        index2 = index;
+        index2 = i2;
         attr1 = attrs1[index];
         attr2 = attrs2[index2];
+        if (!attr2) { break; }
+        display_message(" attr diff for "+ attr1.nodeName + " and " + attr2.nodeName);
         // make the attributes insertion and removal lists.
         if (attr1.nodeName !== attr2.nodeName) {
             // look ahead in the second list until a greater or matching element is found
-            index2 += 1;
+            //index2 += 1;
             while (index2 < attrs2.length) {
                 // if (2) is greater (1) then add this attribute (1) to the removal list.
                 // or if a match, mark it to go through "value comparison"
@@ -70,24 +82,52 @@ jQuery(function($) {
                 attr2 = attrs2[index2];
                 comp = compare_nodeNames(attr2, attr1);
                 if (comp > 0) {
-                    parsed_attr_lists.removal.push({"key":attr1.nodeName, "value":attr1.nodeValue});
-                    break;
+                    parsed_attr_delta_lists.removal.push({"key":attr1.nodeName, "value":attr1.nodeValue});
+                    parsed_attr_delta_lists.insertion.push({"key":attr2.nodeName, "value":attr2.nodeValue});
+                    //break;
                 }
                 else if (comp === 0) {
-                    parsed_attr_lists.key_match.push([{"key":attr1.nodeName, "value":attr1.nodeValue}, {"key":attr2.nodeName, "value":attr2.nodeValue}]);
+                    // nodeNames (aka attribute keys) are the same...
+                    // only add to delta lsit if the values are different. 
+                    if (attr1.nodeValue !== attr2.nodeValue) {
+                        parsed_attr_delta_lists.key_match.push({"key":attr1.nodeName, "values":[attr1.nodeValue, attr2.nodeValue]});
+                    }
+                    // remove from the removal or insertion lists if it was added
+                    pos = key_position(parsed_attr_delta_lists.removal, attr1);
+                    if (pos >= 0) {
+                        parsed_attr_delta_lists.removal.splice(pos, 1);
+                    }
+                    pos = key_position(parsed_attr_delta_lists.insertion, attr1);
+                    if (pos >= 0) {
+                        parsed_attr_delta_lists.insertion.splice(pos, 1);
+                    }
+                    i2 += 1;
                     break;
                 }
                 else {
-                    parsed_attr_lists.insertion.push({"key":attr2.nodeName, "value":attr2.nodeValue});
+                    parsed_attr_delta_lists.insertion.push({"key":attr2.nodeName, "value":attr2.nodeValue});
                 }
                 index2 += 1;
             }
         }
         else {
-            parsed_attr_lists.key_match.push([{"key":attr1.nodeName, "value":attr1.nodeValue}, {"key":attr2.nodeName, "value":attr2.nodeValue}]);
+            if (attr1.nodeValue !== attr2.nodeValue) {
+                parsed_attr_delta_lists.key_match.push({"key":attr1.nodeName, "values":[attr1.nodeValue, attr2.nodeValue]});
+            }
+            // remove from the removal or insertion lists if it was added
+            // remove from the removal or insertion lists if it was added
+            pos = key_position(parsed_attr_delta_lists.removal, attr1);
+            if (pos >= 0) {
+                parsed_attr_delta_lists.removal.splice(pos, 1);
+            }
+            pos = key_position(parsed_attr_delta_lists.insertion, attr1);
+            if (pos >= 0) {
+                parsed_attr_delta_lists.insertion.splice(pos, 1);
+            }
+            i2 += 1;
         }
     }
-    display_message("parsed_attr_lists " + JSON.stringify(parsed_attr_lists, null, " "));
-    return parsed_attr_lists;
+    display_message("parsed_attr_delta_lists " + JSON.stringify(parsed_attr_delta_lists, null, " "));
+    return parsed_attr_delta_lists;
   }
 });
